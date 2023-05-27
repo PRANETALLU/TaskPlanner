d927144948a8dffcbac0a7009007cedd982d3037
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-05-27 16:00:51 -0400

    app setup and header created

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import './css/Header.css';
+import './css/Main.css';
+import './css/Footer.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
